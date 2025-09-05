497f0b8cd777c63046c549dbc304ee528efb2fe9
KokinFR <franciscorafaelzero78@gmail.com>
2025-09-05 16:04:55 -0300

    feat: creating the message for each http status

--- src/main/java/com/example/ms_pet_shop/contracts/ProductConstracts.java
+++ src/main/java/com/example/ms_pet_shop/contracts/ProductConstracts.java
@@ -10,7 +10,7 @@ public class ProductConstracts {
     public static final String PRODUCT_200_DELETED_MESSAGE = "Produto deletado do sistema!";
 
     public static final String PRODUCT_404_MESSAGE = "Esse produto não foi encontrado!";
-    public static final String PRODUCT_DUPLICATE_MESSAGE = "Há um produto que corresponde a essa descrição.";
+    public static final String PRODUCT_DUPLICATE_MESSAGE = "Há um produto que corresponde a essa descrição!";
 
     public static final int PRODUCT_200_STATUS = 200;
     public static final int PRODUCT_201_STATUS = 201;
